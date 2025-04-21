4f18c49f8107cb5b80a6e3ffad8a6ff01fd217af
yaprakbirgul <yaprakbirgul85@gmail.com>
2025-04-21 14:19:25 +0300

    Initial commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,47 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hesap Makinesine Hoşgeldiniz!");
+        System.out.print("Birinci sayıyı girin: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("İkinci sayıyı girin: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("İşlem seçin: +, -, *, /");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else {
+                    System.out.println("Sıfıra bölme hatası!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Geçersiz işlem!");
+                return;
+        }
+
+        System.out.println("Sonuç: " + result);
+    }
+}
+5
